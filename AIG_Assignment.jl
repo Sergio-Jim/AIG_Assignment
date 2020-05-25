8ba8996c6eace6ad62d7db7a8bb06d685f7345fb
Sergio-Jim <56093574+Sergio-Jim@users.noreply.github.com>
2020-05-25 23:51:15 +0200

    Update AIG_Assignment.jl
    Unfortunately only one of us could commit to repository seeing that Ms. Janny David has issues with her github. Not much help could be provided that we work on two different operating systems, Mac and Windows respectively.

--- AIG_Assignment.jl
+++ AIG_Assignment.jl
@@ -1,7 +1,9 @@
-using CSV, DataFrames, Plot, Statistics
-
+using CSV, DataFrames
+"""
+Data cleaning and preparation done by both Jimmy and Janny
+"""
 #Using dataset
-bankDataset = CSV.read("C:\\Users\\jimmy\\Documents\\AIG_Assignment\\bank-additional-full")
+bankDataset = CSV.read("C:\\Users\\jimmy\\Documents\\AIG_Assignment\\bank-additional-full.csv")
 
 #Removing unwanted columns
 select!(bankDataset, Not(:contact))
@@ -9,92 +11,125 @@ select!(bankDataset, Not(:education))
 select!(bankDataset, Not(:default))
 select!(bankDataset, Not(:month))
 select!(bankDataset, Not(:day_of_week))
-select!(bankDataset, Not(:campain))
-#removing 
+select!(bankDataset, Not(:campaign))
 
 #converting dataset to numeric values
 bankDataset = convert(Matrix,bankDataset)
 
 #Choosing your features for X
-X = bankDataset[:, 1:15]
+X = bankDataset[:, 1:end-1]
 
 #Defining the Y values
-Y = bankDataset[:, 16]
+Y = bankDataset[:, end]
 
 X_matrix = convert(Matrix, X)
 
 
 X_train = X[1:58, :]
+X_test = X[58+1:end, :]
 
-X_test = X[59+1:end, :]
-
-Y_train = Y[1:1, :]
-
-Y_test = Y[1+1:end, :]
+Y_train = Y[1:58, :]
+Y_test = Y[58+1:end, :]
 """
 The Dataset has been cleaned and prepared
 """
 
 """
-Implementing the Regularized Logical Regression Algorithm
+Done by Janny David
 """
-#Sigmoid function
-function
-    sigmoid(z)
-    return 1 ./ (1 .+ exp.(.-z))
+"""
+Implementing the Regulizized Logical Regression Algorithm
+"""
+#hypothesis for Logistic Regression with sigmoid function
+function hypothesis(x, θ)
+       z = -θ' * X
+   return 1 / (1 + exp(z))
 end
 
 #regularized cost function
 function
-    regularised_costFunction(X, y, θ, λ)
+    regularised_costFunction(X, Y, θ, λ)
 
 #size of training examples
-       m = length(y)
+       m = length(Y)
 
 #hypothesis of sigmoid function
-       h= sigmoid(X * 0)
+       hypothesis = sigmoid(X * 0)
 
 #when y=1
-       yfunctionIsOne = log.(1 .-h))
+       yfunctionIsOne = ((- Y)' * log.(1 .- hypothesis))
+
+#when Y=0
+       yfunctionIsZero = ((1 .- Y)' * log.(1 .- hypothesis))
 
 #lamba for regulization
-       lamba = (λ/(2*m) * sum(θ[2 : end] .^ 2))
+       regulationTerm = (λ/(2*m) * sum(θ[2 : end] .^ 2))
 
-#Almost forgot WHEN Y=0
-       yfunctionISZero = ((1 .- y)' * log.(1 .- h))
+#Cost function
+       J(θ) = (1/m) * (yFunctionIsOne - yfunctionISZero) + regulationTerm
 
-#cOST function of j theta
-       jParameterThetaCost = (1/m) * (yFunctionIsOne - yfunctionISZero) + lamba
-
-#minimized cost function of parameter thetaa 0
-       rGradientD = (1/m) * (X') * (h-y) + ((1/m) * (λ * θ))
-
-#minimized cost function of parameter thetaa 1 and beyond
-       rGradientD[1] = (1/m) * (X[:, 1])' * (h-y)
-   return (rGradientD, rGradientD[1])
+       #minimized cost function gradient descent for theeta 0
+       θ0 = (1/m) * (X[:, 1])' * (hypothesis - Y)
+       #minimized cost function gradient descent for theeta 1 till theetaa n
+       θj[1] = (1/m) * (X') * (hypothesis - Y) + ((1/m) * (λ * θ))
+    return (θ0, θj[1])
 end
 
-function reg_logistic_regression(X, y, λ, fit_intercept=true, η=0.01, max_iter=1000)
+#iterations for gradient descent
+function logistic_reg(X, Y, λ, fit_intercept=true, η=0.01, iteration = 1000)
 
-#size of training examples
-       m = length(y)
+    #Size of training examples
+    m = length(Y);
 
-#the fit intercept
-       fit_intecept(X)
+    if fit_intercept
+        constant = ones(m, 1)
+        X = hcat(constant, X)
+    else
+        X
+    end
+    #n features to initialize vectors and using the iteration to initialize J
+    n = size(X)[2]
+    θ = zeros(n)
+    J = zeros(iteration)
 
-#n = number of features. used to initialize vector for theetaa
-       n = size(X)[15]
+    for w in range(1, stop = iteration)
 
-#initialized from n
-       θ = zeros(n)
+        J[w], θj = regularised_costFunction(X, y, θ, λ)
+        θ = θ - (η * θj)
+    end
 
-#Getting to initialize the cost vector
-       rGradientD = zeros(max_iter)
-
-#Simultanious update
-       for iter in range(1, stop=max_iter)
-          rGradientD[iter], minJ = regularised_cost(X, y, θ, λ)
-          θ = θ - (η * minJ)
-       end
-   return (θ, minJ)
+    return (θ, J)
 end
+
+"""
+Done by Jimmy Damiao
+"""
+"""
+Classifiers for dataset
+"""
+
+"""
+Accuracy
+"""
+function predict_probability(X, θ, fit_intercept = true)
+    m = size(X)[1]
+
+    if fit_intercept
+        constant = ones(m, 1)
+        X = con(constant, X)
+    elseif fit_intercept!
+        X #when fit is not specified
+    hypothesis = sigmoid(X * θ)
+    return hypothesis
+end
+end
+
+#Binary classifier
+function predictedClass(proba, dBoundary = 0.5)
+    return proba .>= dBoundary
+end
+
+#Check to see if yes or no
+test_score = mean(Y_test .== predictedClass(predict_probability(X_test, θ)));
+#results
+println("Test score: ", round(test_score)
